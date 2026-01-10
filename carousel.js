3f57e2f7cae52c9b621902460986ea8e42e6d2d3
showszs <pasativan152@gmail.com>
2026-01-10 15:20:33 +0300

    slider-class-1.0

--- carousel.js
+++ carousel.js
@@ -1,66 +1,65 @@
-function Carousel() {
-    this._container = document.querySelector('.carousel')
-    this._containerSlides = document.querySelector('.container-slides')
-    this._slides = document.querySelectorAll('.slide')
+class Carousel {
+    constructor() {
+        this._container = document.querySelector('.carousel')
+        this._containerSlides = document.querySelector('.container-slides')
+        this._slides = document.querySelectorAll('.slide')
 
-    this._CODE_ARROW_LEFT = 'ArrowLeft'
-    this._CODE_ARROW_RIGHT = 'ArrowRight'
-    this._CODE_SPACE = 'Space'
-    this._INTERVAL = 2000
+        this._CODE_ARROW_LEFT = 'ArrowLeft'
+        this._CODE_ARROW_RIGHT = 'ArrowRight'
+        this._CODE_SPACE = 'Space'
 
-    this._isPlaying = true
-    this._counterLi = 0
-}
+        this._INTERVAL = 2000
+        this._isPlaying = true
+        this._counterLi = 0
+    }
 
-
-Carousel.prototype = {
     _goToNth(n) {
         this._slides[this._counterLi].classList.toggle('active')
         this._indicatorItems[this._counterLi].classList.toggle('active')
         this._counterLi = (n + this._slides.length) % this._slides.length
         this._slides[this._counterLi].classList.toggle('active')
         this._indicatorItems[this._counterLi].classList.toggle('active')
-    },
+    }
 
     _nextSlide() {
         this._goToNth(this._counterLi + 1)
-    },
+    }
 
     _prevSlide() {
         this._goToNth(this._counterLi - 1)
-    },
+    }
 
     _tick() {
         this._timerId = setInterval(() => this._nextSlide(), this._INTERVAL)
-    },
+    }
 
     _pause() {
         if (!this._isPlaying) return
         this._pauseBtn.textContent = 'Play'
         clearInterval(this._timerId)
         this._isPlaying = false
-    },
+    }
 
     _play() {
         this._pauseBtn.textContent = 'Pause'
         this._tick()
         this._isPlaying = true
-    },
+    }
 
     _playPauseHandler() {
         this._isPlaying ? this._pause() : this._play()
-    },
+    }
 
     _nextButtonSlide() {
         this._pause()
         this._nextSlide()
-    },
+    }
 
     _prevButtonSlide() {
         this._pause()
         this._prevSlide()
 
-    },
+    }
 
     _indicator(e) {
         const { target } = e
@@ -69,7 +68,7 @@ Carousel.prototype = {
             this._pause()
             this._goToNth(+num)
         }
-    },
+    }
 
     _pressKey(e) {
         const { code } = e
@@ -80,18 +79,18 @@ Carousel.prototype = {
             this._playPauseHandler()
         }
 
-    },
+    }
 
     _swipeStart(e) {
         this._startPosX = e instanceof MouseEvent ? e.pageX : e.changedTouches[0].pageX
-    },
+    }
     _swipeEnd(e) {
         this._endPosX = e instanceof MouseEvent ? e.pageX : e.changedTouches[0].pageX
 
         if (this._endPosX - this._startPosX > 100) this._prevButtonSlide()
         if (this._endPosX - this._startPosX < -100) this._nextButtonSlide()
 
-    },
+    }
 
     _initControls() {
         const controls = document.createElement('div')
@@ -105,7 +104,7 @@ Carousel.prototype = {
         this._pauseBtn = document.querySelector('.btn-pause')
         this._prevBtn = document.querySelector('.btn-prev')
         this._nextBtn = document.querySelector('.btn-next')
-    },
+    }
 
     _initIndicators() {
         const indicators = document.createElement('div')
@@ -120,7 +119,7 @@ Carousel.prototype = {
 
         this._indicatorItems = document.querySelectorAll('.indicator')
         this._indicatorsContainer = document.querySelector('.indicators-container')
-    },
+    }
 
     _initListeners() {
         this._pauseBtn.addEventListener('click', this._playPauseHandler.bind(this))
@@ -132,14 +131,12 @@ Carousel.prototype = {
         this._container.addEventListener('touchend', this._swipeEnd.bind(this))
         this._container.addEventListener('mouseup', this._swipeEnd.bind(this))
         document.addEventListener('keydown', this._pressKey.bind(this))
-    },
-
+    }
     init() {
         this._initControls()
         this._initIndicators()
         this._initListeners()
         this._tick()
-    },
+    }
 }
 
-Carousel.prototype.constructor = Carousel
